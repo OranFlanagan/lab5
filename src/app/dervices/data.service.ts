c9a03e89dc653f37f2ab162d706e8eb84a11d181
OranFlanagan <163148816+OranFlanagan@users.noreply.github.com>
2025-03-11 22:29:34 +0000

    first commit

--- src/app/dervices/data.service.ts
+++ src/app/dervices/data.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core'; // Makes this service injectable into components
+import { HttpClient } from '@angular/common/http'; // Allows making HTTP requests
+import { Observable } from 'rxjs'; // Handles asynchronous data streams
 
-
+// Marks this service as injectable and available throughout the app
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private httpClient:HttpClient) { }
-
-  getStudentData():Observable<any>
-  {
-    return this.httpClient.get('https://www.jsonblob.com/api/jsonblog/1347544192825614336')
+  // Inject HttpClient so it can be used to make API requests
+  constructor(private httpClient: HttpClient) { }
+  
+  //gets data from jsonblog api
+  getStudentData(): Observable<any> {
+    return this.httpClient.get('https://www.jsonblob.com/api/jsonblog/1347544192825614336');
   }
 
-  getWeatherData():Observable<any>
-  {
-    return this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303')
+  //gets data for galway weather
+  getWeatherData(): Observable<any> {
+    return this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303');
   }
 }
